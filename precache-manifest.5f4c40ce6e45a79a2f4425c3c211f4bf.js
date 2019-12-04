f64aad69f17603fa27bb3982e8df503d2c7d31a1
soumenpro <soumenbhowmick@hotmail.com>
2019-12-04 07:45:55 +0530

    Updates

---
+++ precache-manifest.5f4c40ce6e45a79a2f4425c3c211f4bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8035c06722477cebd41c5ce4a5def80",
+    "url": "/awesome-kittens/index.html"
+  },
+  {
+    "revision": "f762ecf296b1b69da8c5",
+    "url": "/awesome-kittens/static/css/main.1258e12c.chunk.css"
+  },
+  {
+    "revision": "73b7774b248a30fabee0",
+    "url": "/awesome-kittens/static/js/2.eb096e86.chunk.js"
+  },
+  {
+    "revision": "f762ecf296b1b69da8c5",
+    "url": "/awesome-kittens/static/js/main.4c79350b.chunk.js"
+  },
+  {
+    "revision": "d6d9b801befc8ce37de6",
+    "url": "/awesome-kittens/static/js/runtime-main.86a7960d.js"
+  }
+]);
